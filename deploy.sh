af906064b37603d0b36172cf6214e79eeeed7e41
hzr <hzr19960121@163.com>
2019-11-15 15:22:01 +0800

    update sh

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+repo1=../$1/dist/*
+branch=$2
+cd $repo1 && cp -rf $repo1 && git checkout $branch && git pull && rm -rf * 
+git add . && git commit -m 'deploy'  && git push -u origin $branch && git checkout master
